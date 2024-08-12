6d0effa883624e4ae658649d7164c61bd52df913
Aidan Caulfield <acaulf@optonline.net>
2024-08-12 11:21:14 -0400

    Created main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import panel as pn
+
+class GUI:
+    def __init__(self) -> None:
+        pass
+    def create_layout(self):
+        pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
