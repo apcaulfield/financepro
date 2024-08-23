7d474ffeac9230b485af0437861d242386cca4aa
Aidan Caulfield <acaulf@optonline.net>
2024-08-23 13:00:13 -0400

    Create new user functionality complete

--- data_manager.py
+++ data_manager.py
@@ -3,10 +3,13 @@ import platform
 import logging
 import json
 from pathlib import Path
+from typing import List
+
 import humanize
 import msgspec
 
 logger = logging.getLogger(__name__)
+
 
 class Expense(msgspec.Struct):
     """Describes data associated with an expense."""
@@ -20,13 +23,22 @@ class Expense(msgspec.Struct):
     description: str | None = None
     notes: str | None = None
 
+
 class UserData(msgspec.Struct):
     """Describes the structure of all user JSON data."""
-    
-    expenses: set[Expense] = set()
+
+    expenses: List[Expense] = []
     names: set[str] = set()
     categories: set[str] = set()
     tags: set[str] = set()
+
+
+class UserConfig(msgspec.Struct):
+    """Describes the structure of user config.json files."""
+
+    username: str
+    launches: int
+    data_size: str
 
 
 # class MasterList(msgspec.Struct):
@@ -64,7 +76,9 @@ class DataManager:
     boot_user_config
         User configuration at the time of login.
     new_user_data
-        Dictionary of all new data inputted since login.
+        Dictionary containing all new data inputted since login.
+    combined_user_data
+        Dictionary containing data from boot up and data created during current execution.
 
     """
 
@@ -81,7 +95,7 @@ class DataManager:
         self.username = username
 
         # Find file path of user data based on OS
-        if not self.get_appdata_path():
+        if not self.set_appdata_path():
             # User data folder not found - create it
             self.create_new_user()
 
@@ -95,18 +109,20 @@ class DataManager:
             "tags": [],
         }
 
-    def get_appdata_path(self):
-        """Function that sets the appdata path to save/load user data from."""
+    def set_appdata_path(self) -> bool:
+        """Function that sets the appdata path to save/load user data from.
+        Returns true if the user already exists and returns false if
+        the user app data directory cannot be found."""
 
         # Determine app data path based on OS
         os_type = platform.system()
         if os_type == "Darwin":  # macOS
             self.app_data_dir = os.path.join(
-                os.path.expanduser('~'), "Library", "Application Support", "financepro"
+                os.path.expanduser("~"), "Library", "Application Support", "financepro"
             )
         elif os_type == "Linux":  # Linux
             self.app_data_dir = os.path.join(
-                os.path.expanduser('~'), ".local", "share", "financepro"
+                os.path.expanduser("~"), ".local", "share", "financepro"
             )
         elif os_type == "Windows":  # Windows
             self.app_data_dir = os.path.join(os.getenv("APPDATA"), "financepro")
@@ -125,9 +141,10 @@ class DataManager:
             try:
                 os.makedirs(self.user_app_data_dir)
             except FileExistsError:
-                # TODO: Directory actually does exist
+                # TODO: Directory was created between the time of checking if it exists and making it
                 pass
 
+            # User does not exist
             return False
 
         # User does exist
@@ -139,7 +156,7 @@ class DataManager:
         # Sum sizes of all files in directory and its subdirectories
         total_size = sum(
             f.stat().st_size
-            for f in Path(self.user_app_data_dir).rglob('*')
+            for f in Path(self.user_app_data_dir).rglob("*")
             if f.is_file()
         )
         # Convert total file size to human readable format
@@ -149,33 +166,36 @@ class DataManager:
         """Function that initializes files and data for a new user."""
 
         # Create new data file for user
-        with open(self.user_data_file, 'w') as file:
-            json.dump({}, file)
+        with open(self.user_data_file, "wb") as file:
+            new_user_data = UserData()
+            json_new_user_data = msgspec.json.encode(new_user_data)
+            file.write(json_new_user_data)
 
         # Create new config/info file for user
-        user_data_size = self.get_user_data_size()
-        user_config = {
-            "username": self.username,
-            "launches": 1,
-            "data_size": user_data_size,
-        }
-        with open(
-            os.path.join(self.user_app_data_dir, "user_config.json"), 'w'
-        ) as file:
-            json.dump(user_config, file)
+        with open(self.user_config_file, "wb") as file:
+            user_data_size = self.get_user_data_size()
+            user_config = UserConfig(
+                username=self.username,
+                launches=1,
+                data_size=user_data_size,
+            )
+            json_new_user_config = msgspec.json.encode(user_config)
+            file.write(json_new_user_config)
 
     def load_data(self):
         """Loads user data and config from respective files.
-        Initializes the boot_user_expenses and boot_user_config attributes.
+        Initializes the boot_user_expenses, combined_user_expenses, and boot_user_config attributes.
         """
-        # Load user data file 
-        with open(self.user_data_file, 'r') as file:
-            self.boot_user_data = msgspec.json.decode(file, type=UserData)
+
+        # Load user data file
+        with open(self.user_data_file, "rb") as file:
+            file_contents = file.read()
+            self.boot_user_data = msgspec.json.decode(file_contents, type=UserData)
         # Initialize combined user expenses
         self.combined_user_expenses = self.boot_user_data
 
         # Load user configuration file
-        with open(self.user_config_file, 'r') as file:
+        with open(self.user_config_file, "rb") as file:
             self.boot_user_config = json.load(file)
 
     def save_data(self):
@@ -192,4 +212,5 @@ class DataManager:
 
         """
 
+        # TODO: Add name, category, and tags to respective lists if they are not in them already
         self.new_user_data["expenses"].append(expense)
